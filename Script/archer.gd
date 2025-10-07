5c34311743ae0ac925d74a9cc18bcb3dc45a75ec
Amine <93395428+Kreyzz@users.noreply.github.com>
2025-10-08 00:59:35 +0200

    ajout d'un système d'attaque pour la tourelle

--- Script/archer.gd
+++ Script/archer.gd
@@ -1,60 +1,55 @@
 extends CharacterBody3D
 
 @onready var archer:AnimatedSprite3D = $AnimatedSprite3D
+@onready var detection_area = $Area3D  # Le Area3D avec la sphère de collision
+@onready var arrow_spawn_point = $ArrowSpawnPoint
+
 @export var arrow_scene: PackedScene
 
+var can_shoot = true
+var shoot_cooldown = 2.0
+
 func _ready():
+	
+	# Connecter le signal body_entered de l'Area3D
+	detection_area.body_entered.connect(_on_body_entered)
+	
+	# Optionnel : détecter aussi les Area3D qui entrent
+	detection_area.area_entered.connect(_on_area_entered)
+	
 	archer.play("idle")
-	
-	# debug
-	var dummy = Node3D.new()
-	dummy.global_transform.origin = global_transform.origin + Vector3(0,0,5)  # 5 unités devant
-	get_tree().current_scene.add_child(dummy)
-	
-	target = dummy
-	
-	
-var target: Node3D = null # Stocker l'ennmie visé
-var can_shoot: bool = true # Gérer le cooldown entre chaque tirs
-var shoot_cooldown: float = 1.0 # Nombre de secondes entre chaque flèches tiré
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	if target == null:
-		target = body # L'archer comence à viser ce body
-		print("Nouvelle cible :", target.name) # debug
-
-
-func _on_area_3d_body_exited(body: Node3D) -> void:
-	if target == body:
-		target = null # La cible est sortie de la portée
-		print("Cible perdue") # debug
+# Détecte les corps physiques (RigidBody3D, CharacterBody3D, StaticBody3D, etc.)
+func _on_body_entered(body: Node3D):
+	# Tu peux vérifier le type d'objet si besoin
+	if body.is_in_group("enemy"):  # Exemple avec un groupe
+		shoot_at_target(body)
 		
-func shoot_at_target():
-	if not arrow_scene or not target:
+# Détecte les Area3D qui entrent
+func _on_area_entered(area: Area3D):
+	shoot_at_target(area)
+
+func shoot_at_target(target: Area3D):
+	if not can_shoot:
 		return
 	
-	# Instancier la flèche	
-	var arrow = arrow_scene.instantiate()
-	
-	# Positionner la flèche au niveau de l'archer
-	arrow.global_transform.origin = global_transform.origin + Vector3(0, 1.5, 0)
-	
-	# Calculer la direction vers la cible 
-	var direction = (target.global_transform.origin - arrow.global_transform.origin).normalized()
-	
-	# Ajouter la flèche à la scène
-	get_tree().current_scene.add_child(arrow)
-	
-	# Jouer l'animation "shooting" de l'archer
-	archer.play("shooting")
-	
-	can_shoot = false
-	await get_tree().create_timer(shoot_cooldown).timeout
-	can_shoot = true
-	
-func _process(delta):
-	if target and can_shoot:
-		look_at(target.global_transform.origin, Vector3.UP)
-		shoot_at_target()
-		# debug
-		target = $Dummy
+	if arrow_scene:
+		var arrow = arrow_scene.instantiate()
+		get_parent().add_child(arrow)
+		
+		# Position + rotation + échelle
+		arrow.global_transform.origin = arrow_spawn_point.global_transform.origin
+		arrow.scale = Vector3.ONE
+		
+		# Orientation vers la cible (Area3D)
+		arrow.look_at(target.global_transform.origin, Vector3.UP)
+		
+		if arrow is RigidBody3D:
+			arrow.linear_velocity = arrow.transform.basis.z * -20  # vitesse 20
+		
+		# Animation
+		archer.play("shooting")
+		
+		can_shoot = false
+		await get_tree().create_timer(shoot_cooldown).timeout
+		can_shoot = true
