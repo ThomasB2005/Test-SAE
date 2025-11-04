c1f3f4ed4fd4c2435b8888c4206325f94e170a81
Amine <93395428+AmineBin@users.noreply.github.com>
2025-11-04 14:07:22 +0100

    Clean up archer.gd by removing unused functions
    Removed unused functions related to shooting and damage handling.

--- Script/archer.gd
+++ Script/archer.gd
@@ -6,7 +6,8 @@ var can_shoot = true
 var shoot_cooldown = 2.0
 
 func _ready():
-	maxHealth = 60.0
+	animator = archer # Liaison de la variable générique de la classe mère
+	maxHealth = 60.0 # Initialisation des PV max
 	currentHealth = maxHealth
 	
 	healthBar.max_value = maxHealth
@@ -16,32 +17,3 @@ func _ready():
 	detection_area.area_entered.connect(_on_area_entered)
 	
 	archer.play("idle")
-		
-# Détecte les Area3D qui entrent
-func _on_area_entered(area: Node3D):
-	shoot_at_target(area)
-	
-func take_damage(damage: float):
-	healthBar.value -= damage
-	print("-----------J'AI ", healthBar.value, " HP -------------") # debug
-	if healthBar.value <= 0:
-		can_shoot = false
-		archer.play("death")
-		print("Je suis mort") # debug
-		await get_tree().create_timer(4.0).timeout
-		queue_free()
-	
-
-func shoot_at_target(target: Node3D):
-	if not can_shoot:
-		return
-		
-	while can_shoot:
-		# Animation
-		archer.play("shooting")
-		print("Je tire") # debug
-		await archer.animation_finished
-		archer.play("idle")
-		await get_tree().create_timer(shoot_cooldown).timeout
-		
-		self.take_damage(40.0) # debug
