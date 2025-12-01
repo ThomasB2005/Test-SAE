b4c58921c7fb7a3a08ce5ce50c243e02650d86dd
Amine <93395428+Kreyzz@users.noreply.github.com>
2025-12-01 10:59:02 +0100

    Refonte du menu

--- Script/Menu/settings.gd
+++ Script/Menu/settings.gd
@@ -1,5 +1,8 @@
 extends Control
 @onready var menu = preload("res://Scenes/Menu/menu.tscn")
+@onready var click: AudioStreamPlayer = $Click
 
 func _on_btn_back_button_down() -> void:
+	click.play()
+	await click.finished
 	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
